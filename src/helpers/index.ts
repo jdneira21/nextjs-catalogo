9a17a75d1534e753e04591aec33e126e3093b0ab
jdneira21 <jdneira21@gmail.com>
2023-12-07 02:29:42 -0500

    Update index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,6 +4,6 @@ export const uploadBase64Data = async (slug: string, thumbnail: string) => {
   // console.log('uploadBase64Data')
   const base64Data = thumbnail.replace('data:image/jpeg;base64,', '')
   const x = await writeFile(`public/${slug}.jpg`, base64Data, 'base64')
-  console.log('uploadBase64Data')
+  console.log('uploadBase64Data uploadBase64Data')
   console.log(x)
 }
