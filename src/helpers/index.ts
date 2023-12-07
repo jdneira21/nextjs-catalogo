9976d198c3e03d658e543c7a54bac00c5a1535fc
jdneira21 <jdneira21@gmail.com>
2023-12-07 02:22:37 -0500

    s

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,9 @@
-import { writeFile } from 'fs/promises';
+import { writeFile } from 'fs/promises'
 
 export const uploadBase64Data = async (slug: string, thumbnail: string) => {
+  // console.log('uploadBase64Data')
+  const base64Data = thumbnail.replace('data:image/jpeg;base64,', '')
+  const x = await writeFile(`public/${slug}.jpg`, base64Data, 'base64')
   console.log('uploadBase64Data')
-  const base64Data = thumbnail.replace('data:image/jpeg;base64,', '');
-  await writeFile(`public/${slug}.jpg`, base64Data, 'base64');
-};
+  console.log(x)
+}
