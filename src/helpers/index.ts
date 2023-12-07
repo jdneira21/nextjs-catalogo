65b0ca82f6710796606eb030fafa598ccd2be91e
jdneira21 <jdneira21@gmail.com>
2023-12-07 03:16:36 -0500

    Update index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,11 +1,21 @@
 import { writeFile } from 'fs/promises'
+
+// import { readFileSync } from "fs";
+
 export const uploadBase64Data = async (slug: string, thumbnail: string) => {
   // console.log('uploadBase64Data')
   const base64Data = thumbnail.replace('data:image/jpeg;base64,', '')
   try {
-    const x = await writeFile(`public/${slug}.jpg`, base64Data, 'base64')
-    console.log('uploadBase64Data uploadBase64Data')
-    console.log(process.cwd())
+    // basePath = path.join(process.cwd(), ".next/server/chunks")
+
+    // const templateDirectory = resolve(process.cwd(), 'extensions')
+    // console.log(templateDirectory)
+    // const emailTemplate = readFile(join(templateDirectory, slug, 'README.md'), 'utf8')
+
+    await writeFile(`${process.cwd()}/public/${slug}.jpg`, base64Data, 'base64')
+    // console.log(x)
+    // console.log('uploadBase64Data uploadBase64Data')
+    // console.log(process.cwd())
   } catch (error) {
     console.log(error)
   }
